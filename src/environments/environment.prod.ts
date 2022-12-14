393acb645a0c37cca1541cdab4c97b8379a7b440
Jonisoc <jonathan3492@hotmail.com>
2022-12-13 18:33:06 -0700

    v 1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBzI2XXnZZBWEa6UpV4JFZYMubjL5RlBe8",
+    authDomain: "examenhibridasu4.firebaseapp.com",
+    projectId: "examenhibridasu4",
+    storageBucket: "examenhibridasu4.appspot.com",
+    messagingSenderId: "851396445791",
+    appId: "1:851396445791:web:548b6eff22f01e5c1e3dea"
+  }
 };
